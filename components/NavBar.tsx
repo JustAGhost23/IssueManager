2e689b50e8d5eac72018a0fedd79f509816e6a24
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-11-26 21:40:48 +0530

    feat: add a NavBar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import Link from "next/link";
+import React from "react";
+import { FaBug } from "react-icons/fa6";
+
+const NavBar = () => {
+  return (
+    <nav className="flex h-16 items-center space-x-8 px-4 mb-4 border-b">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-8">
+        <li>
+          <Link
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            href="/"
+          >
+            Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            href="/issues"
+          >
+            Issues
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
