e11ff9bd57f5edef3db9283c73601ddd3844d370
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-11-30 21:12:41 +0530

    refactor: add error handling to client

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import React from "react";
-import { Button, TextField, Callout } from "@radix-ui/themes";
+import { Button, Text, TextField, Callout } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
+import ErrorMessage from "../../../components/ErrorMessage";
+import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "../../zodSchemas";
 
 interface IssueForm {
   title: string;
@@ -16,7 +19,14 @@ interface IssueForm {
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
 
   return (
@@ -39,6 +49,7 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Issue Title" {...register("title")} />
         </TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -46,6 +57,7 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button>Submit New Issue</Button>
       </form>
     </div>
