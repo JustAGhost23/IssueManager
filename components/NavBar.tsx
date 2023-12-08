246089f08200141b7f4614f5ea0167a095881ed7
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-12-08 22:01:22 +0530

    feat: add login button to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { FaBug } from "react-icons/fa6";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   return (
     <nav className="flex h-16 items-center space-x-8 px-4 mb-4 border-b">
@@ -35,6 +38,14 @@ const NavBar = () => {
           </Link>
         </li>
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link href="/api/auth/signout">Log out</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
+      </Box>
     </nav>
   );
 };
