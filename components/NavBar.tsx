0e89a90bd590a692aaef01d5c497422029f52cc3
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-12-10 20:28:25 +0530

    feat: add a dropdown to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,47 +5,80 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { FaBug } from "react-icons/fa6";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
   const { status, data: session } = useSession();
 
   return (
-    <nav className="flex h-16 items-center space-x-8 px-4 mb-4 border-b">
-      <Link href="/">
-        <FaBug />
-      </Link>
-      <ul className="flex space-x-8">
-        <li>
-          <Link
-            className={`${
-              currentPath === "/" ? "text-zinc-900" : "text-zinc-500"
-            } hover:text-zinc-800 transition-colors`}
-            href="/"
-          >
-            Dashboard
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={`${
-              currentPath === "/issues" ? "text-zinc-900" : "text-zinc-500"
-            } hover:text-zinc-800 transition-colors`}
-            href="/issues"
-          >
-            Issues
-          </Link>
-        </li>
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Log out</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
-      </Box>
+    <nav className="px-4 py-4 mb-4 border-b">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <FaBug />
+            </Link>
+            <ul className="flex space-x-8">
+              <li>
+                <Link
+                  className={`${
+                    currentPath === "/" ? "text-zinc-900" : "text-zinc-500"
+                  } hover:text-zinc-800 transition-colors`}
+                  href="/"
+                >
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`${
+                    currentPath === "/issues"
+                      ? "text-zinc-900"
+                      : "text-zinc-500"
+                  } hover:text-zinc-800 transition-colors`}
+                  href="/issues"
+                >
+                  Issues
+                </Link>
+              </li>
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="4"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="4">{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Log out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
