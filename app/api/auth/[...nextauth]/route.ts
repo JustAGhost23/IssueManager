bc8f3bf2ea3c937c74b360f48ce2b469ab55acba
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-12-11 21:16:52 +0530

    refactor: extract authOptions to separate file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,6 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { prisma } from "../../../config/db";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import authOptions from "../auth";
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
