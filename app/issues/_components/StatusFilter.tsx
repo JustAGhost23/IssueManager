6c1215ca3698dae81b01a4d0f4c7e1bacd8ada31
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-12-27 23:16:40 +0530

    feat: use search params for filtering

--- app/issues/_components/StatusFilter.tsx
+++ app/issues/_components/StatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/router";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value?: Status }[] = [
@@ -14,15 +14,22 @@ const statuses: { label: string; value?: Status }[] = [
 
 const StatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(status) => {
-        const query = status ? `?status=${status}` : "";
-        router.push(`/issues` + query);
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+
+        const query = params.size ? "?" + params.toString() : "";
+        router.push("/issues" + query);
       }}
     >
-      <Select.Trigger placeholder="Filter by status" />
+      <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
           <Select.Item key={status.value} value={status.value || ""}>
