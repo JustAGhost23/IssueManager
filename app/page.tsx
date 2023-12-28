a267a57b7c2bf2f4d968326ed485fb7b0a7a4f19
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-12-28 12:06:35 +0530

    feat: add structure to main dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import { prisma } from "./config/db";
+import LatestIssues from "../components/LatestIssues";
+import IssueSummary from "../components/IssueSummary";
 import IssueChart from "../components/IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
 
 export default async function Home() {
   const open = await prisma.issue.count({
@@ -12,5 +15,13 @@ export default async function Home() {
     where: { status: "CLOSED" },
   });
 
-  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="4">
+      <Flex direction="column" gap="4">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 }
