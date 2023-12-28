02e4cdd93ec85c10608ef574528f3370b6fd672b
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-12-28 12:08:10 +0530

    feat: cache fetchUser query

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import { prisma } from "../../config/db";
 import { notFound } from "next/navigation";
 import { Grid, Box, Flex } from "@radix-ui/themes";
+import { cache } from "react";
 import IssueDetails from "./_components/IssueDetails";
 import EditIssueButton from "./_components/EditIssueButton";
 import DeleteIssueButton from "./_components/DeleteIssueButton";
@@ -12,12 +13,14 @@ interface Props {
   params: { id: string };
 }
 
+const fetchUser = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
+
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
 
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchUser(parseInt(params.id));
 
   if (!issue) notFound();
 
@@ -29,7 +32,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex direction="column" gap="4">
-            <AssigneeSelect issue={issue}/>
+            <AssigneeSelect issue={issue} />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
